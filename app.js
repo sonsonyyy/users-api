aea21cff3d60ef20553a55c9aa6ac7d5f7c585e6
Erick Salangsang <salangsangerickson@gmail.com>
2025-11-24 18:02:41 +0800

    Initial Express Setup

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import env from 'dotenv'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+// import connectDB from './config/database.js' // Uncomment this line to enable database connection
+// import usersRouter from './routes/userRoutes.js' // Uncomment this line to enable users routes
+// import errorMiddleware from './middlewares/errorMiddleware.js' // Uncomment this line to enable error handling middleware
+
+env.config()
+
+const app = express()
+const PORT = process.env.API_PORT || 8080
+
+// Setup CORS
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+)
+
+// Middleware
+app.use(bodyParser.json())
+
+// Connect to the database
+// connectDB() // Uncomment this line when database connection is needed
+
+// Routes
+// app.use('/users', usersRouter) // Uncomment this line to enable users routes/
+
+// Error Middleware
+// app.use(errorMiddleware) // Uncomment this line to enable error handling middleware
+
+// Home route
+app.get('/', (req, res) => {
+    res.send('Users API is running')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
