5b37ff0a820a9ed814af55778c11756398703459
Erick Salangsang <salangsangerickson@gmail.com>
2025-11-25 14:30:31 +0800

    Add timestamps on User schema/model

--- models/user.js
+++ models/user.js
@@ -1,19 +1,28 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        }
     },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+    { 
+        timestamps: true
     }
-})
+)
+
+userSchema.pre('findOneAndUpdate', function() {
+    this.set({ updatedAt: new Date() })
+});
 
 export default mongoose.model('User', userSchema)
