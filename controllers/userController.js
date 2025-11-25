79a47cf961bebbf82b22ae2a0e57e14f3ffa90c9
Erick Salangsang <salangsangerickson@gmail.com>
2025-11-25 13:41:19 +0800

    Get user by id endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,17 @@ export const getAllUsers = async (req, res) => {
         return res.status(500).json({ message: 'Server error', error: error.message })
     }
 }
+
+export const getUserById = async (req, res) => {
+    const userId = req.params.id
+
+    try {
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error', error: error.message })
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.post('/', userController.createUser)
 router.get('/', userController.getAllUsers)
+router.get('/:id', userController.getUserById)
 
 export default router
