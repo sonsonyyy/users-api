58ab82c1e319ba05ff4659b1a687bb40c42e84db
Erick Salangsang <salangsangerickson@gmail.com>
2025-11-25 13:36:19 +0800

    Get all users endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,3 +13,12 @@ export const createUser = async (req, res) => {
         return res.status(500).json({ message: 'Server error', error: error.message })
     }
 }
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+        return res.status(200).json(users)
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error', error: error.message })
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ import * as userController from '../controllers/userController.js'
 const router = express.Router()
 
 router.post('/', userController.createUser)
+router.get('/', userController.getAllUsers)
 
 export default router
